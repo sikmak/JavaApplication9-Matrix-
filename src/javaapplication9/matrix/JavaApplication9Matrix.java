c0dc8b84f46c410512a9f3d6f09299120a2ece05
Nikita Kolchanov <sikmakn@gmail.com>
2016-02-09 20:14:12 +0600

    1

---
+++ src/javaapplication9/matrix/JavaApplication9Matrix.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication9.matrix;
+
+/**
+ *
+ * @author Никита
+ */
+public class JavaApplication9Matrix {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
